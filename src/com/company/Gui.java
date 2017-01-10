5ed60ffd412cd4132a5b6106c44d386f3e2bb6c1
MontolioV <vetal07_mail@mail.ru>
2017-01-10 18:13:41 +0200

    Showing possible keyword in descending order by frequency added.

--- src/com/company/Gui.java
+++ src/com/company/Gui.java
@@ -61,19 +61,22 @@ public class Gui {
          */
         @Override
         public void actionPerformed(ActionEvent e) {
+            String nameAndFr;
             DefaultListModel<String> defListModel = new DefaultListModel<String>();
+
             cwCombo.removeAllItems();
             kwList.removeAll();
-            for (String s : findKWs()) {
-                cwCombo.addItem(s);
-                defListModel.addElement(s);
+            for (KeyWordWithFrequency frKW : findKWs()) {
+                nameAndFr = frKW.getName() + " (x" + frKW.getFrequ() + ")";
+                cwCombo.addItem(nameAndFr);
+                defListModel.addElement(nameAndFr);
             }
             kwList.setModel(defListModel);
         }
 
-        private TreeSet<String> findKWs() {
+        private ArrayList<KeyWordWithFrequency> findKWs() {
             HashSet<String> strPull = new HashSet<String>();
-            TreeSet<String> ununiqStrs = new TreeSet<String>();
+            ArrayList<KeyWordWithFrequency> possibleKWs = new ArrayList<KeyWordWithFrequency>();
             ArrayList<String> tmpSal;
             TxtReader tr;
             try {
@@ -88,73 +91,33 @@ public class Gui {
                     tmpSal.addAll(from_txt);
                     for (String s : tmpSal) {
                         if (!strPull.add(s)) {
-                            ununiqStrs.add(s);
+                            KeyWordWithFrequency tmpfrKW = new KeyWordWithFrequency(s);
+                            if (possibleKWs.contains(tmpfrKW)) {
+                                possibleKWs.get(possibleKWs.indexOf(tmpfrKW)).riseFrequ();
+                            } else {
+                                possibleKWs.add(tmpfrKW);
+                            }
                         }
                     }
                 }
+
+                Collections.sort(possibleKWs, Collections.reverseOrder(new KWFrequencyComparator()));
 
                 tr.close_buffer();
             } catch (FileNotFoundException e1) {
                 e1.printStackTrace();
             }
-            return ununiqStrs;
+            return possibleKWs;
         }
     }
 
-    class KWFrequencyComparator implements Comparator<KWwithFrequency> {
-
-        /**
-         * Compares its two arguments for order.  Returns a negative integer,
-         * zero, or a positive integer as the first argument is less than, equal
-         * to, or greater than the second.<p>
-         * <p>
-         * In the foregoing description, the notation
-         * <tt>sgn(</tt><i>expression</i><tt>)</tt> designates the mathematical
-         * <i>signum</i> function, which is defined to return one of <tt>-1</tt>,
-         * <tt>0</tt>, or <tt>1</tt> according to whether the value of
-         * <i>expression</i> is negative, zero or positive.<p>
-         * <p>
-         * The implementor must ensure that <tt>sgn(compare(x, y)) ==
-         * -sgn(compare(y, x))</tt> for all <tt>x</tt> and <tt>y</tt>.  (This
-         * implies that <tt>compare(x, y)</tt> must throw an exception if and only
-         * if <tt>compare(y, x)</tt> throws an exception.)<p>
-         * <p>
-         * The implementor must also ensure that the relation is transitive:
-         * <tt>((compare(x, y)&gt;0) &amp;&amp; (compare(y, z)&gt;0))</tt> implies
-         * <tt>compare(x, z)&gt;0</tt>.<p>
-         * <p>
-         * Finally, the implementor must ensure that <tt>compare(x, y)==0</tt>
-         * implies that <tt>sgn(compare(x, z))==sgn(compare(y, z))</tt> for all
-         * <tt>z</tt>.<p>
-         * <p>
-         * It is generally the case, but <i>not</i> strictly required that
-         * <tt>(compare(x, y)==0) == (x.equals(y))</tt>.  Generally speaking,
-         * any comparator that violates this condition should clearly indicate
-         * this fact.  The recommended language is "Note: this comparator
-         * imposes orderings that are inconsistent with equals."
-         *
-         * @param o1 the first object to be compared.
-         * @param o2 the second object to be compared.
-         * @return a negative integer, zero, or a positive integer as the
-         * first argument is less than, equal to, or greater than the
-         * second.
-         * @throws NullPointerException if an argument is null and this
-         *                              comparator does not permit null arguments
-         * @throws ClassCastException   if the arguments' types prevent them from
-         *                              being compared by this comparator.
-         */
-        @Override
-        public int compare(KWwithFrequency o1, KWwithFrequency o2) {
-            return (o1.getFrequ() - o2.getFrequ());
-        }
-    }
 }
 
-class KWwithFrequency {
+class KeyWordWithFrequency implements Comparable<KeyWordWithFrequency> {
     private String name;
     private int frequ = 0;
 
-    KWwithFrequency(String name) {
+    KeyWordWithFrequency(String name) {
         this.name = name;
     }
 
@@ -173,9 +136,9 @@ class KWwithFrequency {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof KWwithFrequency)) return false;
+        if (!(o instanceof KeyWordWithFrequency)) return false;
 
-        KWwithFrequency that = (KWwithFrequency) o;
+        KeyWordWithFrequency that = (KeyWordWithFrequency) o;
 
         return getName().equals(that.getName());
 
@@ -184,5 +147,96 @@ class KWwithFrequency {
     @Override
     public int hashCode() {
         return getName().hashCode();
+    }
+
+    /**
+     * Compares this object with the specified object for order.  Returns a
+     * negative integer, zero, or a positive integer as this object is less
+     * than, equal to, or greater than the specified object.
+     * <p>
+     * <p>The implementor must ensure <tt>sgn(x.compareTo(y)) ==
+     * -sgn(y.compareTo(x))</tt> for all <tt>x</tt> and <tt>y</tt>.  (This
+     * implies that <tt>x.compareTo(y)</tt> must throw an exception iff
+     * <tt>y.compareTo(x)</tt> throws an exception.)
+     * <p>
+     * <p>The implementor must also ensure that the relation is transitive:
+     * <tt>(x.compareTo(y)&gt;0 &amp;&amp; y.compareTo(z)&gt;0)</tt> implies
+     * <tt>x.compareTo(z)&gt;0</tt>.
+     * <p>
+     * <p>Finally, the implementor must ensure that <tt>x.compareTo(y)==0</tt>
+     * implies that <tt>sgn(x.compareTo(z)) == sgn(y.compareTo(z))</tt>, for
+     * all <tt>z</tt>.
+     * <p>
+     * <p>It is strongly recommended, but <i>not</i> strictly required that
+     * <tt>(x.compareTo(y)==0) == (x.equals(y))</tt>.  Generally speaking, any
+     * class that implements the <tt>Comparable</tt> interface and violates
+     * this condition should clearly indicate this fact.  The recommended
+     * language is "Note: this class has a natural ordering that is
+     * inconsistent with equals."
+     * <p>
+     * <p>In the foregoing description, the notation
+     * <tt>sgn(</tt><i>expression</i><tt>)</tt> designates the mathematical
+     * <i>signum</i> function, which is defined to return one of <tt>-1</tt>,
+     * <tt>0</tt>, or <tt>1</tt> according to whether the value of
+     * <i>expression</i> is negative, zero or positive.
+     *
+     * @param o the object to be compared.
+     * @return a negative integer, zero, or a positive integer as this object
+     * is less than, equal to, or greater than the specified object.
+     * @throws NullPointerException if the specified object is null
+     * @throws ClassCastException   if the specified object's type prevents it
+     *                              from being compared to this object.
+     */
+    @Override
+    public int compareTo(KeyWordWithFrequency o) {
+        return this.getName().compareTo(o.getName());
+    }
+}
+
+class KWFrequencyComparator implements Comparator<KeyWordWithFrequency> {
+
+    /**
+     * Compares its two arguments for order.  Returns a negative integer,
+     * zero, or a positive integer as the first argument is less than, equal
+     * to, or greater than the second.<p>
+     * <p>
+     * In the foregoing description, the notation
+     * <tt>sgn(</tt><i>expression</i><tt>)</tt> designates the mathematical
+     * <i>signum</i> function, which is defined to return one of <tt>-1</tt>,
+     * <tt>0</tt>, or <tt>1</tt> according to whether the value of
+     * <i>expression</i> is negative, zero or positive.<p>
+     * <p>
+     * The implementor must ensure that <tt>sgn(compare(x, y)) ==
+     * -sgn(compare(y, x))</tt> for all <tt>x</tt> and <tt>y</tt>.  (This
+     * implies that <tt>compare(x, y)</tt> must throw an exception if and only
+     * if <tt>compare(y, x)</tt> throws an exception.)<p>
+     * <p>
+     * The implementor must also ensure that the relation is transitive:
+     * <tt>((compare(x, y)&gt;0) &amp;&amp; (compare(y, z)&gt;0))</tt> implies
+     * <tt>compare(x, z)&gt;0</tt>.<p>
+     * <p>
+     * Finally, the implementor must ensure that <tt>compare(x, y)==0</tt>
+     * implies that <tt>sgn(compare(x, z))==sgn(compare(y, z))</tt> for all
+     * <tt>z</tt>.<p>
+     * <p>
+     * It is generally the case, but <i>not</i> strictly required that
+     * <tt>(compare(x, y)==0) == (x.equals(y))</tt>.  Generally speaking,
+     * any comparator that violates this condition should clearly indicate
+     * this fact.  The recommended language is "Note: this comparator
+     * imposes orderings that are inconsistent with equals."
+     *
+     * @param o1 the first object to be compared.
+     * @param o2 the second object to be compared.
+     * @return a negative integer, zero, or a positive integer as the
+     * first argument is less than, equal to, or greater than the
+     * second.
+     * @throws NullPointerException if an argument is null and this
+     *                              comparator does not permit null arguments
+     * @throws ClassCastException   if the arguments' types prevent them from
+     *                              being compared by this comparator.
+     */
+    @Override
+    public int compare(KeyWordWithFrequency o1, KeyWordWithFrequency o2) {
+        return (o1.getFrequ() - o2.getFrequ());
     }
 }
